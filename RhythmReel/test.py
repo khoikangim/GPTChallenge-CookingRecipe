8c497af063d526586a743d8df0b25f08678302dd
khoikangim <lina0430@naver.com>
2023-11-09 15:30:08 +0900

    main edit

---
+++ RhythmReel/test.py
@@ -0,0 +1,13 @@
+
+def greet(name):
+    print(f"Hello, {name}!")
+
+if __name__ == "__main__":
+    user_name = input("Enter your name: ")
+    greet(user_name)
+
+    # 추가된 부분: 특정 조건에 따른 메시지 출력
+    if len(user_name) > 5:
+        print("That's a long name!")
+    else:
+        print("Nice to meet you!")
