a20718446d37c1c0813fffe7357e490eec3bac7e
khoikangim <lina0430@naver.com>
2023-11-09 16:18:04 +0900

    커밋 featureB

--- RhythmReel/test.py
+++ RhythmReel/test.py
@@ -12,12 +12,9 @@ if __name__ == "__main__":
     if len(user_name) > 5:
         print("That's a long name!")
     else:
-<<<<<<< HEAD
-        print("Nice to meet you")
-=======
         print("Nice to meet you!")
 
     # 나이를 입력받아 출력하는 부분 추가
     user_age = input("Enter your age: ")
-    print(f"You are {user_age} years old.")
->>>>>>> 38f5a04e0bcda47a43b0da596a0395ae7487b026
+    print("You are {user_age} years old.")
+
